60ab5dc8975fdc167dd6b8ef3d6186f6d9b60cb1
Rahel <rahelasgedom19@gmail.com>
2021-07-26 12:36:33 -0400

    test case part 1 is done

---
+++ src/ResetPractice.java
@@ -0,0 +1,4 @@
+public class ResetPractice {
+
+    // Test case part 1 is done
+}
--- src/SecondClass.java
+++ src/SecondClass.java
@@ -6,5 +6,6 @@ public class SecondClass {
         //work 2 is done
         //work 3 is done here
         //work 4 is done here
+
     }
 }
