420f59e03d1e04e6bf9e323fd9ade881ccb98550
Rahel <rahelasgedom19@gmail.com>
2021-07-26 12:52:46 -0400

    Test cases all done

--- src/ResetPractice.java
+++ src/ResetPractice.java
@@ -6,5 +6,9 @@ public class ResetPractice {
    // Test case part 4 is done
    // Test case part 5 is done
     //Test case part 3 is done
+    //Test case part 4 is done
+    //Test case part 5 is done
+    //Mixed/Soft reset remove the commit from histroy but keeps the local changes
+    // Did part 4 and 5 and I am done in one commit
 
 }
