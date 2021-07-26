4e9bfac9452d374b101cedcea83e129f5f4691fc
Rahel <rahelasgedom19@gmail.com>
2021-07-26 12:38:01 -0400

    test case part 2 is done

--- src/ResetPractice.java
+++ src/ResetPractice.java
@@ -1,4 +1,8 @@
 public class ResetPractice {
 
     // Test case part 1 is done
+   // Test case part 2 is done
+    //Test case part 3 is done
+   // Test case part 4 is done
+   // Test case part 5 is done
 }
